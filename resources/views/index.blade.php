20458fac94fad20542b6b482abe1ac7cf4f5fa21
Pravin Bhalekar <developer.pravinbhalekar@gmail.com>
2020-07-07 17:11:03 +0530

    initial commit by pravin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,7 +29,6 @@
 
         @yield('content')
     </body>
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js"></script>
